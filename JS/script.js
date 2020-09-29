dc4f48240f416e461572f17cb32cd5c210a5da48
Dženita Pobrić <dzenita.pobric@edu.fit.ba>
2020-09-29 16:08:23 +0200

    brojači, tim, animacija

--- JS/script.js
+++ JS/script.js
@@ -54,3 +54,17 @@ function w3_open() {
 function w3_close() {
     mySidebar.style.display = "none";
 }
+
+
+
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
