ef6bd3cb41e77d8cccdce77f25fbf0a5df13e4d2
Dženita Pobrić <dzenita.pobric@edu.fit.ba>
2020-10-01 15:38:07 +0200

    data-aos tekst, animacije

--- JS/script.js
+++ JS/script.js
@@ -71,6 +71,12 @@ $('.count').each(function () {
 
 
 
-
-
-
+function aos_init() {
+  AOS.init({
+    duration: 600,
+    once: true
+  });
+}
+$(window).on('load', function() {
+  aos_init();
+});
